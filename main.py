20bfcfdf3235414f1e1aafc9f54e2c5759cd218f
clementmlin <clement8.moulin@gmail.com>
2025-10-06 12:14:08 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+'''
+on import ce qu'il faut
+'''
 #### Imports et définition des variables globales
 
 FILENAME = "listes.csv"
@@ -5,47 +8,89 @@ FILENAME = "listes.csv"
 #### Fonctions secondaires
 
 def read_data(filename):
-    """retourne le contenu du fichier <filename>
-
-    Args:
-        filename (str): nom du fichier à lire
-
-    Returns:
-        list: le contenu du fichier (1 list par ligne)
-    """
-    l = []
-    return l
+    '''
+    on import ce qu'il faut
+    '''
+    data = []
+    with open(filename, 'r', encoding='utf8') as f:
+        for line in f:
+            line = line.strip()
+            if ';' in line:
+                parts = line.split(';')
+            elif ',' in line:
+                parts = line.split(',')
+            else:
+                parts = line.split()
+            nums = []
+            for p in parts:
+                p = p.strip()
+                nums.append(int(p))
+            if nums:
+                data.append(nums)
+    return data
 
 def get_list_k(data, k):
-    l = []
+    '''
+    on import ce qu'il faut
+    '''
+    l=data[k]
     return l
 
 def get_first(l):
-    return None
+    '''
+on import ce qu'il faut
+'''
+    return l[0]
 
 def get_last(l):
-    return None
+    '''
+on import ce qu'il faut
+'''
+    return l[-1]
 
 def get_max(l):
-    return None
+    '''
+on import ce qu'il faut
+'''
+    maximum=l[0]
+    for i in l[1:]:
+        if i>maximum:
+            maximum=i
+    return maximum
 
 def get_min(l):
-    return None
+    '''
+on import ce qu'il faut
+'''
+    minimum=l[0]
+    for i in l[1:]:
+        if i<minimum:
+            minimum=i
+    return minimum
 
 def get_sum(l):
-    return None
+    '''
+on import ce qu'il faut
+'''
+    s=0
+    for i in range(len(l)):
+        s+=l[i]
+    return s
 
 
 #### Fonction principale
 
 
 def main():
+    '''
+on import ce qu'il faut
+'''
     pass
-    # data = read_data(FILENAME)
-    # for i, l in enumerate(data):
-    #     print(i, l)
-    # k = 37
-    # print(k, get_list_k(data, 37))
+    data = read_data(FILENAME)
+    for i, l in enumerate(data):
+        print(i, l)
+    k = 37
+    print(k, get_list_k(data, 37))
 
 
 if __name__ == "__main__":
